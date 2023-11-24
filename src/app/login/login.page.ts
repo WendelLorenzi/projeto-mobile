7fa1b8957e072b61c5a1ad3da6b8bb2fbaac4db4
gui <guigon@alunos.utfpr.edu.br>
2023-11-24 10:59:33 -0300

    feat: add a simple login verification

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -7,11 +7,21 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  public email!: string;
+  public password!: string;
+
   constructor(private router: Router) {}
 
   ngOnInit() {}
 
   IrParaRegistrar() {
     this.router.navigate(['registro']);
+  }
+
+  Login() {
+    console.log(this.email);
+    console.log(this.password);
+    if (this.email == 'teste@gmail.com' && this.password == 'projeto-final')
+      this.router.navigate(['folder/Eventos']);
   }
 }
